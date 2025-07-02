7bb9d5a7459694991d7f306af7671292aa62790d
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-07-02 18:24:02 +0100

    Create script.js

---
+++ JavaScript/Variables and Strings/Greeting Bot/Trivia Bot/script.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Winterr";
+const botLocation = "Remote";
+const favoriteLanguage = "Python";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is the most popular programming language.";
+
+console.log(codingFact)
+
+codingFact = favoriteLanguage + " the most common language among machine learning repositories and is the third most common language on GitHub overall.";
+
+console.log(codingFact)
+
+codingFact = favoriteLanguage + " is generally considered one of the easier programming languages to learn, especially for beginners.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");

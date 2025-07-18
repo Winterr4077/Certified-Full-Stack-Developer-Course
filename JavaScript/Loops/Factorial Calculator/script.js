612abe265f8e21b589c642fe39e922e72c27593c
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-07-18 14:26:04 +0100

    Create script.js

---
+++ JavaScript/Loops/Factorial Calculator/script.js
@@ -0,0 +1,18 @@
+/* 
+A factorial is the product of an integer and all the integers below it. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120. In this lab, you will create a factorial calculator that takes a number from the user and calculates the factorial of that number.
+*/
+
+let num = 5;
+
+function factorialCalculator(number) {
+    let result = 1;
+    for (let i = 1; i <= number; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+let factorial = factorialCalculator(num);
+
+let resultMsg = `Factorial of ${num} is ${factorial}`;
+console.log(resultMsg);

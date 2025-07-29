e9ab3667ba29272b99182592c3afe5b5fa96cb26
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-07-29 16:05:25 +0100

    Create script.js

---
+++ JavaScript/Functions/Boolean Check Function/script.js
@@ -0,0 +1,11 @@
+function booWho(boolean){
+  if (boolean === true || boolean === false){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(booWho(true))
+console.log(booWho(false))
+console.log(booWho(1))

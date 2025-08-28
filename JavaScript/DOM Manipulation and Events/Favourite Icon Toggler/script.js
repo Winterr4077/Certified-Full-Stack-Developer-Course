0e33dd96120863fb5eff4e340e3f812e3ddbde20
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-08-28 17:00:29 +0100

    Create script.js

---
+++ JavaScript/DOM Manipulation and Events/Favourite Icon Toggler/script.js
@@ -0,0 +1,12 @@
+const favButtons = document.querySelectorAll(".favorite-icon");
+
+favButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    if (button.classList.contains("filled")) {
+      button.classList.remove("filled");
+      button.innerHTML = "&#9825;"; 
+    } else {
+      button.classList.add("filled");
+      button.innerHTML = "&#10084;"; 
+  });
+});
